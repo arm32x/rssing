fdd3014371de4c6d9afb0001f187766862815fdd
arm32x <github.account@arm32.ax>
2025-10-01 16:06:52 -0400

    Remove "An Otherworldly Scholar"

--- main.rkt
+++ main.rkt
@@ -23,10 +23,6 @@
 
 (define feeds
   (list
-    (reddit-rss-feed #:slug     "an-otherworldly-scholar"
-                     #:title    "An Otherworldly Scholar"
-                     #:username "ralo_ramone")
-
     (reddit-rss-feed #:slug     "dungeon-life"
                      #:title    "Dungeon Life"
                      #:username "Khenal")
